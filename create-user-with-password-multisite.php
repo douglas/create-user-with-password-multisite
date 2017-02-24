5bd39bf87aab92cd8974ce60c15c9a9fb328f5ee
Douglas Soares de Andrade <contato@douglasandrade.com>
2017-02-24 14:21:28 -0300

    Updating the plugin file and removing unneeded code for now

--- create-user-with-password-multisite.php
+++ create-user-with-password-multisite.php
@@ -4,11 +4,12 @@
   Plugin Name: Create User With Password Multisite
   Plugin URI: http://www.mooveagency.com
   Description: Add ability to register user with password on WP multisite installation.
-  Version: 1.0.5.
-  Author: Jakub Glos
+  Version: 1.0.6.
+  Author: Jakub Glos / Douglas Soares de Andrade
   Author URI: http://www.mooveagency.com
   License:
   Text Domain: create-user-with-password-multisite
+  Domain Path: /languages
  */
 
 // no need on cron job
@@ -16,44 +17,17 @@ if (defined('DOING_CRON') || isset($_GET['doing_wp_cron'])) {
     return;
 }
 
+/**
+ * Loads the plugin translations
+ */
+add_action('init', 'cuwp_load_translations');
+function cuwp_load_translations() {
+    load_plugin_textdomain('create-user-with-password-multisite', false, dirname(plugin_basename(__FILE__)) . '/languages'); 
+}
 
 // fire in administration only
 if (is_admin()) {
     require_once( 'php/cuwp.php' );
     $mdu = new CUWP_Create_User_With_Password();
 }
-
-/**
- * Install
- */
-function cuwp_activate() {
-    // store old message in option
-    $old_message = get_site_option('welcome_user_email');
-    update_option('cuwp_welcome_user_email', $old_message);
-
-    // set new message
-    $text_var = 'Dear User, \n' .
-            'Thank you for the registration. Please check the email address provided for login details. \n' .
-            '--The Team @ SITE_NAME \n';
-    $text = __($text_var, 'create-user-with-password-multisite');
-
-    update_site_option('welcome_user_email', $text);
-}
-
-register_activation_hook(__FILE__, 'cuwp_activate');
-
-/**
- * Deactivation
- */
-function cuwp_deactivate() {
-    // set old message back
-    $old_message = get_option('cuwp_welcome_user_email');
-    update_site_option('welcome_user_email', $old_message);
-
-    // delete option with the message
-    delete_option('cuwp_welcome_user_email');
-}
-
-register_deactivation_hook(__FILE__, 'cuwp_deactivate');
-
 
