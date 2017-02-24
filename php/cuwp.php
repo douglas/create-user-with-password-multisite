5b4d60ba9a944fc5997fa4a2c43f9da034fe3e6f
Douglas Soares de Andrade <contato@douglasandrade.com>
2017-02-24 14:24:49 -0300

    Upgrading the plugin for Wordpress 4.7.2
    - Upgrading the codebase for wordpress 4.7.2
    - Removing notification email code (hope to add again in the future)
    - Remove the activation key if adding the user with password, as it may
    bring problems if you remove newly added users and try to add them
    again in a short period of time.

--- php/cuwp.php
+++ php/cuwp.php
@@ -3,6 +3,7 @@
 /**
  * Main plugin class
  * */
+
 class CUWP_Create_User_With_Password {
 
     /**
@@ -20,10 +21,6 @@ class CUWP_Create_User_With_Password {
 
         // listen for REQUEST
         add_action('admin_action_createuser', array($this, 'cuwp_listen'), 3);
-
-
-        // remove filter that updates welcome email
-        remove_filter('site_option_welcome_user_email', 'welcome_user_msg_filter');
     }
 
     /**
@@ -41,6 +38,15 @@ class CUWP_Create_User_With_Password {
         <table class="form-table hook-pass">
             <tbody>
             <input type="hidden" name="cuwp_security" value="cuwp" />
+
+            <tr>
+                <th scope="row"><label for="noactivationkey"><?php _e('Delete the user activation key', 'create-user-with-password-multisite'); ?></label></th>
+                <td>
+                    <input type="checkbox" name="noactivationkey" id="noactivationkey" value="1" checked="checked">
+                    <label for="noactivationkey"><?php _e('As the user is created and activated with a new password, there is no need to keep the activation key stored in the database.', 'create-user-with-password-multisite'); ?></label>
+                </td>
+            </tr>
+
             <tr class="form-field form-required">
                 <th scope="row"><label for="cuwp_pass1"><?php _e('Password', 'create-user-with-password-multisite'); ?> <span class="description"><?php _e('(required)'); ?></span></label></th>
                 <td><input name="cuwp_pass1" type="password" id="pass1" autocomplete="off" /><input class="hidden" value=" " /></td>
@@ -51,13 +57,8 @@ class CUWP_Create_User_With_Password {
                 <td><input name="cuwp_pass2" type="password" id="pass2" autocomplete="off" /></td>
             </tr>
 
-            <tr>
-                <td>
-                    <div class="pass-error">
-
-                        <?php _e("Passwords do not match.", 'create-user-with-password-multisite'); ?>
-                    </div>
-                </td>
+            <tr class="pass-error">
+                <td><?php _e("Passwords do not match.", 'create-user-with-password-multisite'); ?></td>
             </tr>
         </tbody>
         </table>
@@ -76,9 +77,14 @@ class CUWP_Create_User_With_Password {
             }
             
             global $wpdb;
-            check_admin_referer('create-user', '_wpnonce_create-user');
-            if (!current_user_can('create_users'))
-                wp_die(__('Cheatin&#8217; uh?'));
+            check_admin_referer( 'create-user', '_wpnonce_create-user' );
+            if ( ! current_user_can( 'create_users' ) ) {
+                wp_die(
+                    '<h1>' . __( 'Cheatin&#8217; uh?' ) . '</h1>' .
+                    '<p>' . __( 'Sorry, you are not allowed to create users.' ) . '</p>',
+                    403
+                );
+            }
 
             if (!is_multisite()) {
                 $user_id = edit_user();
@@ -94,67 +100,53 @@ class CUWP_Create_User_With_Password {
                     die();
                 }
             } else {
-                $user_details = wpmu_validate_user_signup($_REQUEST['user_login'], $_REQUEST['email']);
-                if (is_wp_error($user_details['errors']) && !empty($user_details['errors']->errors)) {
-                    $add_user_errors = $user_details['errors'];
+                $new_user_email = wp_unslash( $_REQUEST['email'] );
+                $new_user_password = wp_unslash( $_REQUEST['cuwp_pass1'] );
+                $user_details = wpmu_validate_user_signup( $_REQUEST['user_login'], $new_user_email );
+
+                if ( is_wp_error( $user_details[ 'errors' ] ) && !empty( $user_details[ 'errors' ]->errors ) ) {
+                    $add_user_errors = $user_details[ 'errors' ];
                 } else {
                     /**
-                     * Filter the user_login, also known as the username, before it is added to the site.
+                     * Filters the user_login, also known as the username, before it is added to the site.
                      *
                      * @since 2.0.3
                      *
                      * @param string $user_login The sanitized username.
                      */
-                    $new_user_login = apply_filters('pre_user_login', sanitize_user(wp_unslash($_REQUEST['user_login']), true));
-                    if (isset($_POST['noconfirmation']) && is_super_admin()) {
-                        add_filter('wpmu_signup_user_notification', '__return_false'); // Disable confirmation email
+                    $new_user_login = apply_filters( 'pre_user_login', sanitize_user( wp_unslash( $_REQUEST['user_login'] ), true ) );
+                    
+                    if ( isset( $_POST[ 'noconfirmation' ] ) && current_user_can( 'manage_network_users' ) ) {
+                        add_filter( 'wpmu_signup_user_notification', '__return_false' ); // Disable confirmation email
+                        add_filter( 'wpmu_welcome_user_notification', '__return_false' ); // Disable welcome email
                     }
-                    wpmu_signup_user($new_user_login, $_REQUEST['email'], array('add_to_blog' => $wpdb->blogid, 'new_role' => $_REQUEST['role']));
-                    if (isset($_POST['noconfirmation']) && is_super_admin()) {
-                        $key = $wpdb->get_var($wpdb->prepare("SELECT activation_key FROM {$wpdb->signups} WHERE user_login = %s AND user_email = %s", $new_user_login, $_REQUEST['email']));
-                        wpmu_activate_signup($key);
-                        $redirect = add_query_arg(array('update' => 'add'), 'user-new.php');
+
+                    wpmu_signup_user( $new_user_login, $new_user_email, array( 'add_to_blog' => $wpdb->blogid, 'new_role' => $_REQUEST['role'] ) );
+                    if ( isset( $_POST[ 'noconfirmation' ] ) && current_user_can( 'manage_network_users' ) ) {
+                        $key = $wpdb->get_var( $wpdb->prepare( "SELECT activation_key FROM {$wpdb->signups} WHERE user_login = %s AND user_email = %s", $new_user_login, $new_user_email ) );
+                        $new_user = wpmu_activate_signup( $key );
+
+                        if (isset( $_POST[ 'noactivationkey' ] )) {
+                            // As the user is created and activated with a new password, there
+                            // is no need to keep the activation key stored in the database.
+                            $wpdb->delete( $wpdb->signups, array( 'activation_key' => $key ) );
+                        }
+
+                        if ( is_wp_error( $new_user ) ) {
+                            $redirect = add_query_arg( array( 'update' => 'addnoconfirmation' ), 'user-new.php' );
+                        } else {
+                            $redirect = add_query_arg( array( 'update' => 'addnoconfirmation', 'user_id' => $new_user['user_id'] ), 'user-new.php' );
+                        }
                     } else {
-                        $redirect = add_query_arg(array('update' => 'add'), 'user-new.php');
+                        $redirect = add_query_arg( array('update' => 'newuserconfirmation'), 'user-new.php' );
                     }
 
                     // set password for user
-                    $user = get_user_by('email', sanitize_email($_REQUEST['email']));
-                    wp_set_password(sanitize_text_field($_REQUEST['cuwp_pass1']), $user->ID);
+                    $user = get_user_by('email', sanitize_email($new_user_email));
+                    wp_set_password(sanitize_text_field($new_user_password), $user->ID);
 
-                    if (isset($_POST['noconfirmation']) && is_super_admin()) {
-                        // send email with login details
-                        $email = 'Dear User,'. '<br/>' .
-                                'Your new account has been set up.'. '<br/>' .
-                                '<br/>' .
-                                'You can log in with the following information:'. '<br/>' .
-                                'Username: %1$s' . '<br/>' .
-                                'Password: %2$s' . '<br/>' .
-                                '<br/>' .
-                                '%3$s ' . '<br/>' .
-                                '<br/>' .
-                                'Thanks!'. '<br/>';
-
-                        $replaced_all = sprintf(__($email, 'create-user-with-password-multisite'), sanitize_user(wp_unslash($_REQUEST['user_login']), true), sanitize_text_field($_REQUEST['cuwp_pass1']), get_admin_url());
-
-                        $headers = 'From: ' . get_option('admin_email') . "\r\n" .
-                                'Content-type: text/html; charset=utf-8\n' .
-                                'Reply-To: noreply@noreply.com' . "\r\n" .
-                                'X-Mailer: PHP/' . phpversion();
-
-                        $mail = wp_mail(sanitize_text_field($_REQUEST['email']), __('Login details', 'create-user-with-password-multisite'), $replaced_all, $headers);
-                        if (true == $mail) {
-                            wp_redirect($redirect);
-                        } else {
-                            wp_die(__('We are sorry but an error has occurred whilst sending the email with the login details. Please deactivate the "Create User with Password Multisite" plugin and contact us via email to resolve this issue: plugins@mooveagency.com', 'create-user-with-password-multisite'));
-                        }
-
-                        die();
-                    } else {
-
-                        wp_redirect($redirect);
-                        die();
-                    }
+                    wp_redirect( $redirect );
+                    die();
                 }
             }
         }
